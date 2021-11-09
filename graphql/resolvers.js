15b2f699f60208018c979a26b62ad5044b8928cf
hiagrawal <agrawal.hina13@gmail.com>
2021-11-09 16:59:42 +0530

    Adding a Login Query and a Resolver

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,21 +1,19 @@
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
 module.exports = {
-
     helloSample() {
         return 'Hello World'
     },
-
     hello() {
         return {
             text: 'Hello World',
             views: 12345
         }
-    }
-    
+    }   
 }
 
 module.exports = {
@@ -58,5 +56,26 @@ module.exports = {
         });
         const createdUser = await user.save();
         return {...createdUser._doc, _id: createdUser._id.toString()};
+    },
+    login: async function({email, password}){
+        const user = await User.findOne({email: email});
+        if(!user){
+            const error = new Error('User not found');
+            error.code = 401;
+            throw error;
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+        if(!isEqual){
+            const error = new Error('Password is incorrect');
+            error.code = 401;
+            throw error;
+        }
+        const token = jwt.sign({
+            userId: user._id.toString(),
+            email: user.email
+        }, 'somesupersecretsecret', {expiresIn: '1h'});
+
+        return {token: token, userId: user._id.toString()};
     }
+
 };
--- graphql/schema.js
+++ graphql/schema.js
@@ -67,6 +67,11 @@ module.exports = buildSchema(`
         posts: [Post!]!
     }
 
+    type AuthData{
+        token: String!
+        userId: String!
+    }
+
     input UserInputData {
         email: String!
         name: String!
@@ -74,7 +79,7 @@ module.exports = buildSchema(`
     }
 
     type RootQuery {
-        hello: String!
+        login(email: String!, password: String!): AuthData!
     }
 
     type RootMutation {
